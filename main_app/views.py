f55d844d6235df90b92d79982edece728778c422
Marvin Merida <marvinmerida@Marvins-MBP.fios-router.home>
2022-07-06 01:43:07 -0400

    templates added

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,7 @@ def home(request):
   return HttpResponse('<h1>Home</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About SongCollector</h1>')
+  return render(request, 'about.html')
 
 def songs_index(request):
   return HttpResponse('<h1>Songs from the SongCollector</h1>')
