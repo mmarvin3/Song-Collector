5075b0b4ad505a589e3c4d70bd832628daf14617
Marvin Merida <marvinmerida@Marvins-MBP.fios-router.home>
2022-07-05 21:26:42 -0400

    defined view functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Home</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About SongCollector</h1>')
+
+def songs_index(request):
+  return HttpResponse('<h1>Songs from the SongCollector</h1>')
